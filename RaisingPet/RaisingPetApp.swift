c500c0d293ff6d737bcef4dd8c79e9af6e9c2bfa
Enes Talha Uçar <talhauenes@gmail.com>
2024-07-29 14:41:37 +0300

    Project Created

---
+++ RaisingPet/RaisingPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaisingPetApp.swift
+//  RaisingPet
+//
+//  Created by Enes Talha Uçar  on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct RaisingPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
