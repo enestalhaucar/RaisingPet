1b488c7aaaf9aae459b2e26ae6ad70bd4b0ff7e3
Enes Talha Uçar <talhauenes@gmail.com>
2024-07-29 14:51:43 +0300

    Requirements of Firebase downloaded

--- RaisingPet/RaisingPetApp.swift
+++ RaisingPet/RaisingPetApp.swift
@@ -4,14 +4,24 @@
 //
 //  Created by Enes Talha Uçar  on 29.07.2024.
 //
-
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct RaisingPetApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashView()
         }
     }
 }
